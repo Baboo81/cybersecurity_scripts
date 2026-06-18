abe4492a19c44dde593d699a8e477e59d36c53e5
Baboo81 <magicbaboo@gmail.com>
2026-06-18 15:33:48 +0200

    mise à jour

--- cartographie_network.py
+++ cartographie_network.py
@@ -17,24 +17,24 @@ for i in range(1, 255):
     # Construction de l'adresse IP complète
     ip = network + str(i)
 
-    # Commande ping adaptée au système d'exploitation
+    # Commande ping adaptée au système d'exploitation 
     if os_name == "Windows":
         command = ["ping", "-n", "1", "-w", "1000", ip]
         # -n 1    → envoie 1 paquet
-        # -w 1000 → timeout en millisecondes
+        # -w 1000 → timeout (en millisecondes)
     else:
         command = ["ping", "-c", "1", "-W", "1", ip]
         # -c 1 → 1 paquet
-        # -W 1 → timeout en secondes
+        # -W 1 → timeout (en secondes)
 
-    # Exécution de la commande ping sans afficher la sortie
+    # Exécution de la commande ping sans afficher la sortie 
     result = subprocess.run(
         command,
         stdout=subprocess.DEVNULL,
         stderr=subprocess.DEVNULL
     )
 
-    # Si returncode == 0, la machine répond
+    # Si returncode == 0, la machine répond ;
     if result.returncode == 0:
         print(f"🟢 {ip} est actif")
         active_hosts.append(ip)
